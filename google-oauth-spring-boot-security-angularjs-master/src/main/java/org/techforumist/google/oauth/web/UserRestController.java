e8de9c1b8875a9ea2c3787f0a94f70c3a88943ac
MJN <maxime.janicot@bluescale.com>
2020-01-09 15:32:48 +0100

    ajout conversion user id user

--- google-oauth-spring-boot-security-angularjs-master/src/main/java/org/techforumist/google/oauth/web/UserRestController.java
+++ google-oauth-spring-boot-security-angularjs-master/src/main/java/org/techforumist/google/oauth/web/UserRestController.java
@@ -3,7 +3,10 @@ package org.techforumist.google.oauth.web;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.techforumist.google.oauth.model.User;
@@ -33,5 +36,9 @@ public class UserRestController {
 		return principal.getName();
 
 	}
-
+	@GetMapping("/user/{id}")
+	@PreAuthorize("hasRole('USER')")
+	public User getUserInfo(Principal principal,@PathVariable Long id){
+		return userRepository.findOne(id);
+	}
 }
